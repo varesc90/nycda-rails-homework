9ecc2be9f031f7163f9647fc040036903c4cc528
Vares Chai <varesc@gmail.com>
2016-09-02 15:22:01 -0400

    submit homework-4

--- homework-4/problem-1/triangle.rb
+++ homework-4/problem-1/triangle.rb
@@ -1,12 +1,22 @@
 class Triangle
 	def initialize (num1,num2,num3)
-		sideone = num1
-		sidetwo = num2
-		sidethree = num3
+		@@sideone = num1
+		@@sidetwo = num2
+		@@sidethree = num3
 	end
 
 	def kind
-		return :isosceles
+		if (@@sideone == @@sidetwo) && (@@sideone == @@sidethree)
+			return :equilateral
+		elsif (@@sideone != @@sidetwo) && (@@sidetwo == @@sidethree)
+			 return :isosceles
+		elsif (@@sideone == @@sidethree) && (@@sideone != @@sidetwo)
+			  return :isosceles
+		elsif (@@sideone == @@sidetwo) && (@@sidetwo != @@sidethree)
+			  return :isosceles
+		elsif (@@sideone != @@sidetwo) && (@@sidetwo != @@sidethree) && (@@sideone != @@sidethree)
+			  return :scalene
+		end
 	end
 
 end
