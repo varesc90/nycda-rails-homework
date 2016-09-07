9c8cb9cd12de0fb845d54c8da8a5db6bcc2764ff
Vares Chai <varesc@gmail.com>
2016-09-07 18:27:49 -0400

    create a new rails project for HW 5

---
+++ homework-5/problem-1/problem-1.rb
@@ -0,0 +1,25 @@
+####EBAY Schema
+
+#####Users#############
+#id - interger		  #
+#userid - string	  #
+#password - string    #
+#firstname - string
+#lastname - string
+#create_at - datetime
+#######################
+#
+#One to many
+#
+#
+#
+##Items
+#id - interger
+#item_name - string
+#price - interger
+#
+#
+#
+#
+#
+#
